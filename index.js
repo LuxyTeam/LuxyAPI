1c8918e806e1ab9d4e8faa7f918149ee81d4f408
Felix <luxyteam404@gmail.com>
2024-09-09 00:40:34 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,13 @@
-const sideMenu = document.querySelector("aside");
-const menuBtn = document.getElementById("menu-btn");
-const closeBtn = document.getElementById("close-btn");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const darkMode = document.querySelector(".dark-mode");
-const lightModeIcon = darkMode.querySelector("span:nth-child(1)");
-const darkModeIcon = darkMode.querySelector("span:nth-child(2)");
-
-// Function to set dark mode
-const setDarkMode = (isDarkMode) => {
-    document.body.classList.toggle("dark-mode-variables", isDarkMode);
-    lightModeIcon.classList.toggle("active", !isDarkMode);
-    darkModeIcon.classList.toggle("active", isDarkMode);
-    localStorage.setItem("darkMode", isDarkMode);
-};
-
-// Function to initialize the state
-const initializeState = () => {
-    const savedDarkMode = localStorage.getItem("darkMode");
-    const isDarkMode = savedDarkMode === "true";
-    setDarkMode(isDarkMode);
-};
-
-// Initialize the state when the page loads
-initializeState();
-
-menuBtn.addEventListener("click", () => {
-    sideMenu.style.display = "block";
-    localStorage.setItem("sideMenu", "block");
+// Definir una ruta para la API que responde con "Hola Mundo"
+app.get('/', (req, res) => {
+  res.send('Hola Mundo');
 });
 
-closeBtn.addEventListener("click", () => {
-    sideMenu.style.display = "none";
-    localStorage.setItem("sideMenu", "none");
-});
-
-darkMode.addEventListener("click", () => {
-    const isDarkMode = !document.body.classList.contains("dark-mode-variables");
-    setDarkMode(isDarkMode);
-});
-
-// Restore the side menu state
-const savedSideMenu = localStorage.getItem("sideMenu");
-if (savedSideMenu) {
-    sideMenu.style.display = savedSideMenu;
-}
-
-Orders.forEach((order) => {
-    const tr = document.createElement("tr");
-    const trContent = `
-        <td>${order.productName}</td>
-        <td>${order.productMethod}</td>
-        <td>${order.endpoint}</td>
-        <td>${order.paymentStatus}</td>
-        <td class="${order.status === "Declined" ? "danger" : order.status === "Pending" ? "warning" : "primary"}">${order.status}</td>
-        <td class="primary">Details</td>
-    `;
-    tr.innerHTML = trContent;
-    document.querySelector("table tbody").appendChild(tr);
+// Iniciar el servidor en el puerto 3000
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
